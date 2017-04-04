ed2d73c0a8f61394d92330620e3e75935f17fcbb
BasementProgramming <BasementProgramming@gmail.com>
2017-04-04 11:34:04 -0500

    Solution to Day 1 Part 2 Completed.

--- AdventOfCode/Day1/Program.cs
+++ AdventOfCode/Day1/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+
+
+/*
+   Below is my solution to the first day of the Advent of Code 2016. The Adevent of Code is an online programming 
+   challenge that features a series of complex problems that can be solved using computer programming. The challenge
+   is not language specific, however, my solutions will all be written in C#.
+
+    Advent of Code: https://adventofcode.com/2016
+*/
 
 namespace Day1
 {
@@ -30,10 +37,13 @@ namespace Day1
                 myMap.Walk(myCompass.Turn(input[0]), int.Parse(input.Substring(1)));
             }
 
-            // Write distance from the origin out to the window
-            Console.WriteLine(string.Format("Day 1 Challenge 1 Distance from your Starting Location: {0} blocks.", myMap.DistanceFromOrigin()));
+            // Write distance from the origin out to the window, the solution to the first part of the challenge
+            Console.WriteLine(string.Format("Day 1 Challenge 1 Result: {0} blocks.", myMap.DistanceFromOrigin()));
 
-            // Lave window open to read result
+            // Write distance from the origin of the first location visited twice out to the window, the solution to the second part of the challenge
+            Console.WriteLine(string.Format("Day 1 Challenge 2 Result: {0} blocks.", myMap.FirstLocationVistedTwice()));
+
+            // Leave window open to read result
             Console.Read();
         }
 
@@ -43,10 +53,15 @@ namespace Day1
             private int xAxis;
             private int yAxis;
 
+            // Use for tracking the locations you have visited for the second part of the challenge
+            private List<Tuple<int, int>> tracker;
+
+
             public MagicMap()
             {
                 xAxis = 0;
                 yAxis = 0;
+                tracker = new List<Tuple<int, int>>();
             }
 
             /*
@@ -55,27 +70,46 @@ namespace Day1
              */
             public void Walk(char direction, int steps)
             {
-                switch (direction)
+                // Loops through the steps and update your tracker for the second part of the challenge
+                for (int step = 0; step < steps; step++)
                 {
-                    case 'N':
-                        xAxis = xAxis + steps;
-                        break;
-                    case 'S':
-                        xAxis = xAxis - steps;
-                        break;
-                    case 'E':
-                        yAxis = yAxis + steps;
-                        break;
-                    case 'W':
-                        yAxis = yAxis - steps;
-                        break;
+                    switch (direction)
+                    {
+                        case 'N':
+                            xAxis++;
+                            break;
+                        case 'S':
+                            xAxis--;
+                            break;
+                        case 'E':
+                            yAxis++;
+                            break;
+                        case 'W':
+                            yAxis--;
+                            break;
+                    }
+
+                    // Update tracker for second part of challenge
+                    tracker.Add(Tuple.Create(xAxis, yAxis));
                 }
             }
+
+
 
             // Calculate how far you are from the origin, make all negative distances postive before getting the sum
             public int DistanceFromOrigin()
             {
-                return (System.Math.Abs(xAxis) + System.Math.Abs(yAxis));
+                return (Math.Abs(xAxis) + Math.Abs(yAxis));
+            }
+
+            // Get the number of blocks away you are from the origin when you vist your first location twice
+            public int FirstLocationVistedTwice()
+            {
+                // Get the coordinate of the first location that you visited twice
+                var duplicateCoordinate = tracker.GroupBy(x => new { x.Item1, x.Item2 }).Where(x => x.Count() > 1).FirstOrDefault();
+
+                // Calculate how far the coordinate is away from the origin, make all negative distances positive before getting the sum
+                return (Math.Abs(duplicateCoordinate.Key.Item1) + Math.Abs(duplicateCoordinate.Key.Item2));
             }
         }
 
